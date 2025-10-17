72b5da8fc8fa6cb90d9d014534a34cb30acf0650
Ryann <maitre.ryann35@gmail.com>
2025-10-17 10:16:07 +0200

    Keep pushing

--- src/main.c
+++ src/main.c
@@ -4,32 +4,50 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
+	if (argc < 3) {
+		printf("Erreur de parametres");
+		return 0;
+	}
+
 	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
 	double r = 0;
+
+	if (strcmp(op, "car") == 0) {
+		if (argc != 3) {
+			printf("Erreur de parametres");
+			return 0;
+		}
+		r = _car(atof(argv[2]));
+		printf("%lf", r);
+		return 0;
+	}
+
+	if (argc != 4) {
+		printf("Erreur de parametres");
+		return 0;
+	}
+
+	double a = atof(argv[2]);
+	double b = atof(argv[3]);
+
 	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
+		r = _add(a, b);
+		printf("%lf", r);
 	}
 	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
+		r = _sub(a, b);
+		printf("%lf", r);
 	}
 	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
+		r = _mul(a, b);
+		printf("%lf", r);
 	}
 	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
+		r = _div(a, b);
+		printf("%lf", r);
 	}
-	else {printf("Erreur de parametres");}
+	else {
+		printf("Erreur de parametres");
 	}
-	else {printf("Erreur de parametres");}
 	return 0;
 }
-
-		
